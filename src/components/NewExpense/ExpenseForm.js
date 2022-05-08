fa270681166f6dea93d8815856814558c3d274dc
Sourav Mandal <sm227465@gmail.com>
2022-05-08 18:41:15 +0530

    Added all components

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expanseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expanseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="">Title</label>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
+        </div>
+
+        <div className="new-expense__control">
+          <label htmlFor="">Amount</label>
+          <input
+            type="number"
+            min="1"
+            step="1"
+            onChange={amountChangeHandler}
+            value={enteredAmount}
+          />
+        </div>
+
+        <div className="new-expense__control">
+          <label htmlFor="">Date</label>
+          <input
+            type="date"
+            min="2019-01-10"
+            max="2030-01-01"
+            onChange={dateChangeHandler}
+            value={enteredDate}
+          />
+        </div>
+      </div>
+
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
---
+++ src/expensesData.js
@@ -0,0 +1,99 @@
+export const myExpenses = [
+  {
+    id: 'e1',
+    title: 'Dell 15.6 inch laptop backpack (Black)',
+    amount: 432,
+    date: new Date('2019, 01, 05'),
+  },
+
+  {
+    id: 'e2',
+    title: 'Mi Basic Wired Headset with Mic (Black)',
+    amount: 399,
+    date: new Date('2019, 03, 17'),
+  },
+
+  {
+    id: 'e3',
+    title: 'HP 32GB Pendrive (Grey)',
+    amount: 475,
+    date: new Date('2019, 04, 03'),
+  },
+
+  {
+    id: 'e4',
+    title: 'Mi 10000 mAh Power Bank (Black)',
+    amount: 899,
+    date: new Date('2019, 05, 02'),
+  },
+
+  {
+    id: 'e5',
+    title: 'Huawei E8372 Data Card (White)',
+    amount: 3195,
+    date: new Date('2019, 07, 06'),
+  },
+
+  {
+    id: 'e6',
+    title: 'KM-206W Wireless Optical Mouse (Silver)',
+    amount: 429,
+    date: new Date('2019, 12, 21'),
+  },
+
+  {
+    id: 'e7',
+    title: 'NH-10 15.6 inch Inch Laptop Backpack (Black)',
+    amount: 689,
+    date: new Date('2020, 01, 21'),
+  },
+
+  {
+    id: 'e8',
+    title: 'NB Gully Cricket Tennis Willow Poplar Bat',
+    amount: 353,
+    date: new Date('2020, 07, 03'),
+  },
+
+  {
+    id: 'e9',
+    title: 'GUBB Toe Nail Clipper (Silver)',
+    amount: 149,
+    date: new Date('2020, 07, 08'),
+  },
+
+  {
+    id: 'e10',
+    title: 'SET WET Cool, Charm and Swag Avatar Deodorant Spray',
+    amount: 244,
+    date: new Date('2020, 08, 10'),
+  },
+
+  {
+    id: 'e11',
+    title: 'POCO M2 Pro (Green and Greener, 64 GB)',
+    amount: 12117,
+    date: new Date('2020, 10, 20'),
+  },
+
+  {
+    id: 'e12',
+    title: 'Turbo Wireless Mouse (Silver)',
+    amount: 379,
+    date: new Date('2020, 11, 07'),
+  },
+
+  {
+    id: 'e13',
+    title: 'Flipkart SmartBuy 1000 W Dry Iron (White-Gray)',
+    amount: 649,
+    date: new Date('2021, 01, 12'),
+  },
+
+  {
+    id: 'e14',
+    title: 'Mi XXQ02HM Trimmer (Black)',
+    amount: 999,
+    date: new Date('2021, 01, 28'),
+  },
+];
